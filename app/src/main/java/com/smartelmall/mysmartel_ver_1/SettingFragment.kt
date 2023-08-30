f38af5d1bc8fa5406ac24185dd51d97d08448fa9
Bansj <bsj98@smartel.com>
2023-08-30 20:46:06 +0900

    앱 푸시 승인 권한 설정으로 넘어가는 버튼 생성, 기능 구현 완료

--- app/src/main/java/com/smartelmall/mysmartel_ver_1/SettingFragment.kt
+++ app/src/main/java/com/smartelmall/mysmartel_ver_1/SettingFragment.kt
@@ -3,6 +3,7 @@ package com.smartelmall.mysmartel_ver_1
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -14,6 +15,7 @@ import android.widget.Switch
 import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
 import com.smartelmall.mysmartel_ver_1.NewPW.IdentificationSelfActivity
+import android.provider.Settings
 
 class SettingFragment : Fragment() {
 
@@ -94,6 +96,13 @@ class SettingFragment : Fragment() {
                     editor.apply()
                 }
             }
+        }
+        // 알림 권한설정 클릭 이벤트
+        val btnPermit: ImageButton = view.findViewById(R.id.btn_permit)
+        btnPermit.setOnClickListener {
+            val intent = Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS)
+            intent.putExtra(Settings.EXTRA_APP_PACKAGE, requireContext().packageName)
+            startActivity(intent)
         }
 
         return view
